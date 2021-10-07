830c0537aaad09cdb983e315cf32f720b69fbc0b
srikarthiksmart <srikarthiksmart@gmail.com>
2021-10-07 15:27:22 +0530

    first Git project

---
+++ src/org/rigthtclick/RighCick.java
@@ -0,0 +1,22 @@
+package org.rigthtclick;
+
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class RighCick {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Success\\eclipse-workspace\\day26 rightclik\\Chromedriver\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/intl/en-GB/gmail/about/#");
+		WebElement signin = driver.findElement(By.xpath("//a[@data-action='sign in']"));
+		Actions rightclick = new Actions(driver);
+		rightclick.contextClick(signin).perform();	}
+
+}
---
+++ src/org/robot/RobotFunction.java
@@ -0,0 +1,70 @@
+package org.robot;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import javax.xml.xpath.XPath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class RobotFunction {
+
+	public static void main(String[] args) throws AWTException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Success\\eclipse-workspace\\day26 rightclik\\Chromedriver\\chromedriver.exe");
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://en-gb.facebook.com/");
+		WebElement username = driver.findElement(By.id("email"));
+//		Actions action=new Actions(driver); 
+//		action.contextClick(username).perform();
+		Robot robot = new Robot();
+		
+		//keyborad actin 
+		robot.keyPress(KeyEvent.VK_3);
+		robot.keyRelease(KeyEvent.VK_3);
+		robot.keyPress(KeyEvent.VK_4);
+		robot.keyRelease(KeyEvent.VK_4);
+		robot.keyPress(KeyEvent.VK_5);
+		robot.keyRelease(KeyEvent.VK_5);
+		
+//		//keyboard action
+//		robot.keyPress(KeyEvent.VK_DOWn);
+//		robot.keyRelease(KeyEvent.VK_DOWN);
+//		
+//		
+//		robot.keyPress(KeyEvent.VK_DOWN);
+//		robot.keyRelease(KeyEvent.VK_DOWN);
+//		//enter 
+//		
+//	robot.keyPress(KeyEvent.VK_ENTER);
+//		robot.keyRelease(KeyEvent.VK_ENTER);
+//		
+//		for (int i = 1; i <=2; i++) {
+//			robot.keyPress(KeyEvent.VK_DOWN);
+//			robot.keyRelease(KeyEvent.VK_DOWN);
+//			
+//			robot.keyPress(KeyEvent.VK_ENTER);
+//			robot.keyRelease(KeyEvent.VK_ENTER);
+//				
+//		}
+//		
+		
+		
+		
+		
+		
+		//signin.click();
+
+	
+
+
+}
+
+
+
+
+}
